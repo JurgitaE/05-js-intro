7ad17a8f67afbf22381463b70ef5f7999188a2a8
Jurgita <jurgita.eicinaite@yahoo.com>
2022-12-12 09:04:15 +0200

    comment

--- js/01-labas.js
+++ js/01-labas.js
@@ -1,4 +1,7 @@
 //  npm i - D nodemon
 // priristi failus prie package json
 console.log(777);
-console.log("Labas rytas");
+console.log('Labas rytas');
+
+// Vienos eilutes komentaras
+/* Keliu eiluciu komentaras */
