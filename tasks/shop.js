6c45264860abec378a43a209c045869da613d7d5
Jurgita <jurgita.eicinaite@yahoo.com>
2022-12-14 12:07:44 +0200

    shop: 0 units in stock

--- tasks/shop.js
+++ tasks/shop.js
@@ -17,6 +17,11 @@ const prekes = [
         price: 1.57,
         inStock: 30,
     },
+    {
+        name: "Arbuzas",
+        price: 1,
+        inStock: 0,
+    },
 ];
 
 function shop(prekes, valiuta) {
@@ -43,3 +48,29 @@ shop(prekes, valiuta);
 // 3) Pomidoras kainuoja 1.57 EUR ir turime ju 30 vienetu.
 // ----------------
 // Viso asortimento kaina: 97.10 EUR.
+function shop2(prekes, valiuta) {
+    console.log("MUSU PARDUOTUVE:");
+    console.log(" ----------------");
+    let kaina = 0;
+    for (let i = 0; i < prekes.length; i++) {
+        if (prekes[i].inStock === 0) {
+            console.log(
+                `${prekes[i].name} kainuoja ${prekes[i].price.toFixed(
+                    2
+                )} ${valiuta}. Prekes neturime sandelyje.`
+            );
+            kaina += prekes[i].price * prekes[i].inStock;
+        } else {
+            console.log(
+                `${prekes[i].name} kainuoja ${prekes[i].price.toFixed(
+                    2
+                )} ${valiuta} ir ju turime ${prekes[i].inStock} vienetu.`
+            );
+            kaina += prekes[i].price * prekes[i].inStock;
+        }
+    }
+    console.log("----------------");
+    console.log(`Viso asortimento kaina: ${kaina.toFixed(2)} ${valiuta}.`);
+}
+
+shop2(prekes, valiuta);
