9913a559808045b92139571b343b413ff05866b6
Jurgita <jurgita.eicinaite@yahoo.com>
2022-12-15 19:16:13 +0200

    mini: 5. function

--- js/homework.js
+++ js/homework.js
@@ -179,13 +179,85 @@ function didziausiasSkaiciusSarase(numList) {
     return maxNum;
 }
 
-console.log(didziausiasSkaiciusSarase([1]));
-console.log(didziausiasSkaiciusSarase([1, 2, 3]));
-console.log(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]));
-console.log(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]));
-console.log(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]));
-console.log(didziausiasSkaiciusSarase("pomidoras"));
-console.log(didziausiasSkaiciusSarase([]));
+// console.log(didziausiasSkaiciusSarase([1]));
+// console.log(didziausiasSkaiciusSarase([1, 2, 3]));
+// console.log(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]));
+// console.log(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]));
+// console.log(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]));
+// console.log(didziausiasSkaiciusSarase("pomidoras"));
+// console.log(didziausiasSkaiciusSarase([]));
+
+/* 
+5. Funkcija pavadinimu “isrinktiRaides”:
+priima du kintamuosius:
+-pirmasis nurodo tekstą, su kuriuo reikės atlikti užduotį
+-antrasis nurodo kas kelintą raidę išrinkti
+
+patikrinti, ar pirmasis kintamasis yra teksto tipo:
+jei ne, išvedame pranešimą “Pirmasis kintamasis yra netinkamo tipo.”
+
+priešingu atveju tęsiame darbą
+patikrinti, ar pirmasis kintamasis yra ne tuščias tekstas ir ne didesnis nei 100 simbolių:
+
+-jei ne, išvedame pranešimą “Pirmojo kintamojo reikšmė yra netinkamo dydžio.”
+
+priešingu atveju tęsiame darbą
+patikrinti, ar antrasis kintamasis yra skaičiaus tipo:
+
+- jei ne, išvedame pranešimą “Antrasis kintamasis yra netinkamo tipo.”
+priešingu atveju tęsiame darbą
+- patikriname, ar antrojo kintamojo vertė yra didesnė už nulį:
+jei ne, išvedame pranešimą “Antrasis kintamasis turi būti didesnis už nulį.”
+
+priešingu atveju tęsiame darbą
+patikriname, ar antrojo kintamojo vertė yra ne didesnė už pirmojo kintamojo ilgį:
+- jei ne, išvedame pranešimą “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+
+priešingu atveju tęsiame darbą
+išrenkame iš nurodyto teksto kas kelintą raidę (pagal antrojo kintamojo žingsnį)
+išrinktas raides sudėti į atskirą kintamąjį, kuris yra teksto tipo
+gražina rezultatą
+TESTAI:
+console.log( isrinktiRaides( “abcdefg”, 2 ) );
+rezultatas: “bdf”
+console.log( isrinktiRaides( “abcdefghijkl”, 3 ) );
+rezultatas: “cfil”
+console.log( isrinktiRaides( “abc”, 0 ) );
+rezultatas: “Antrasis kintamasis turi būti didesnis už nulį.”
+console.log( isrinktiRaides( “abc”, 4 ) );
+rezultatas: “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+console.log( isrinktiRaides( 1561, 2 ) );
+rezultatas: “Pirmasis kintamasis yra netinkamo tipo.”
+ */
+
+function isrinktiRaides(str, everyN) {
+    if (typeof str !== "string") {
+        return `Pirmasis kintamasis yra netinkamo tipo.`;
+    }
+    if (str.length === 0 || str.length > 100) {
+        return `Pirmojo kintamojo reikšmė yra netinkamo dydžio.`;
+    }
+    if (typeof everyN !== "number") {
+        return `Antrasis kintamasis yra netinkamo tipo.`;
+    }
+    if (everyN <= 0) {
+        return `Antrasis kintamasis turi būti didesnis už nulį.`;
+    }
+    if (everyN > str.length) {
+        return `Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.`;
+    }
+    let newString = "";
+    for (let i = everyN - 1; i < str.length; i += everyN) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+console.log(isrinktiRaides("abcdefg", 2));
+console.log(isrinktiRaides("abcdefghijkl", 3));
+console.log(isrinktiRaides("abc", 0));
+console.log(isrinktiRaides("abc", 4));
+console.log(isrinktiRaides(1561, 2));
 
 console.log("-----Ciklo for panaudojimas--------");
 // 1. Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
