0b9748e95d670e5d854adfba0bc260dea7dfaf96
Jurgita <jurgita.eicinaite@yahoo.com>
2022-12-26 23:25:34 +0200

    update

--- js/11-ternary.js
+++ js/11-ternary.js
@@ -1,10 +1,10 @@
 console.clear();
-/* 
-TERNARY (supaprastintas if'as)
-*/
+/*
+    TERNERY (supaprastintas if'as)
+    (if) klausimas ? ka daryti kai true : ka daryti kai false;
+    */
 
 // IF variantas
-
 const laimejo = false;
 let pinigine = 10;
 
@@ -18,19 +18,43 @@ console.log(pinigine);
 
 // IF supaprastintas variantas
 const pergale = false;
-let zinute = "Sveikinu, tu laimejai";
+let zinute = "Sveikinu, tu laimejai!";
 
 if (!pergale) {
-    zinute = "Ups... Bandykite kita karta!";
+    zinute = "Ups.. Bandykite kita karta!";
 }
 
 console.log(zinute);
 
 // TERNARY
-const lotteryWin = true;
+const lotteryWin = false;
 const wallet = 10 + (lotteryWin ? 20 : -1);
 console.log(wallet);
 
-const msgWin = true;
-const msg = true ? "Sveikinu" : "Ups...";
+const msgWin = false;
+const msg = msgWin ? "Sveikinu!" : "Ups...";
 console.log(msg);
+
+console.log("----------------");
+// COMPLEX TERNARY
+
+const a = 0 < 1 ? 2 : 3;
+console.log(a);
+
+const b = 0 > 1 ? 2 : 3;
+console.log(b);
+
+const c = 0 === 1 ? 2 : 3;
+console.log(c);
+
+const d = 0 < 1 ? 2 : 3 ? 4 : 5;
+console.log(d);
+
+const e = 0 > 1 ? 2 : 3 ? 4 : 5;
+console.log(e);
+
+const f = 0 < 1 ? (2 ? 3 : 4) : 5;
+console.log(f);
+
+const g = 0 > 1 ? (2 ? 3 : 4) : 5 ? 6 : 7;
+console.log(g);
--- tasks/testing.js
+++ tasks/testing.js
@@ -1,95 +1,3 @@
-/* console.clear();
-
-console.log("--------DIVIDING WITHOUT REMAINDER----------");
-
-function divideWithoutRemainder(range, dividers) {
-    console.log("--------------------------------------------------------------------------------");
-
-    if (!validateRange(range)) {
-        return;
-    }
-
-    if (!validateDividers(dividers)) {
-        return;
-    }
-
-    let rangeStart = range[0];
-    let rangeEnd = range[1];
-    let timesDivides = 0;
-
-    if (rangeStart <= rangeEnd) {
-        for (let j = 0; j < dividers.length; j++) {
-            for (let i = rangeStart; i <= rangeEnd; i++) {
-                if (i % dividers[j] === 0) {
-                    timesDivides++;
-                }
-            }
-            console.log(`Skaičių intervale tarp ${rangeStart} ir ${rangeEnd}, besidalijančių be liekanos iš ${dividers[j]} yra ${timesDivides} vienetai.`);
-            timesDivides = 0;
-        }
-    } else {
-        for (let j = 0; j < dividers.length; j++) {
-            for (let i = rangeStart; i >= rangeEnd; i--) {
-                if (i % dividers[j] === 0) {
-                    timesDivides++;
-                }
-            }
-            console.log(`Skaičių intervale tarp ${rangeStart} ir ${rangeEnd}, besidalijančių be liekanos iš ${dividers[j]} yra ${timesDivides} vienetai.`);
-            timesDivides = 0;
-        }
-    }
-}
-
-function validateRange(range) {
-    console.log("Validating range...");
-    if (range.length !== 2) {
-        console.log(` [${range}] is a bad range. Only [START, END] , no more, no less. Please fix it.`);
-        return false;
-    } else if (typeof range[0] !== "number" || typeof range[1] !== "number") {
-        console.log(range, "is a bad range. One or both  of the elements is not a NUMBER. Please fix it.");
-        return false;
-    } else if (!isFinite(range[0]) || !isFinite(range[1])) {
-        console.log(range, "is a bad range. One or both of the elements not Finite. Please fix it.");
-        return false;
-    } else if (range[0] % 1 !== 0 || range[1] % 1 !== 0) {
-        console.log(range, "is a bad range. One or both of the elements not Integer. Please fix it.");
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function validateDividers(dividers) {
-    console.log("Validating dividers...");
-    if (dividers.length === 0) {
-        console.log(dividers, "There must be at least 1 divider. Please fix it.");
-        return false;
-    }
-    for (let i = 0; i < dividers.length; i++) {
-        if (typeof dividers[i] !== "number") {
-            console.log(dividers[i], "in", dividers, "is a bad divider.It has to be a NUMBER. Please fix it.");
-            return false;
-        } else if (!isFinite(dividers[i])) {
-            console.log(dividers[i], "in", dividers, "is a bad divider.It cant be Infinite. Please fix it.");
-            return false;
-        } else if (dividers[i] % 1 !== 0) {
-            console.log(dividers[i], "in", dividers, "is a bad divider.It have to be Integral. Please fix it.");
-            return false;
-        }
-    }
-
-    return true;
-}
-
-const range1 = [0, 11];
-// const range2 = [8, 31];
-// const range3 = [-18, 18];
-const dividers = [];
-
-divideWithoutRemainder(range1, dividers);
-// divideWithoutRemainder(range2, dividers);
-// divideWithoutRemainder(range3, dividers);
- */
 console.clear();
 /* 
 For example: ["3:1", "2:2", "0:1", ...]
@@ -141,14 +49,12 @@ console.clear();
 // line(3);
 // line(4);
 console.clear();
-
-function solution(number) {
-    if (number <= 2) return 0;
-
-    return Array(number - 3)
-        .fill(3)
-        .map((a, index, arr) => (arr[index - 1] ? arr[index - 1] + index : a))
-        .filter((a) => a % 3 === 0 || a % 5 === 0)
-        .reduce((acc, cur) => acc + cur, 0);
+function dontGiveMeFive(start, end) {
+    return Array(end - start + 1)
+        .fill(0)
+        .map((a, index) => start + index)
+        .filter((a) => !("" + a).includes("5")).length;
 }
-console.log(solution(10));
+
+dontGiveMeFive(1, 9);
+console.log(dontGiveMeFive(4, 17));
