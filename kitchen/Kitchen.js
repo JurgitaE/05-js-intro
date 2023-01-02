0ffc7d291ee493f367ba9089feba62fe5c5bb2ef
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-02 19:15:09 +0200

    turimiProduktai, sunaudota, sugedesProduktas

--- kitchen/Kitchen.js
+++ kitchen/Kitchen.js
@@ -10,7 +10,18 @@ class Kitchen {
         this.bought.push({ product: pr, units: quant });
     }
     turimiProduktai() {
-        for (let i = 0; i < this.bought.length; i++) {}
+        for (let i = 0; i < this.bought.length; i++) {
+            let { product, units } = this.bought[i];
+            console.log(`${i + 1}) ${product[0].toUpperCase() + product.slice(1)}: ${units}`);
+        }
+    }
+    sunaudota(pr, quant) {
+        const product = this.bought.find((a) => a.product === pr);
+        product.units -= quant;
+    }
+    sugedesProduktas(pr) {
+        const product = this.bought.find((a) => a.product === pr);
+        this.bought.splice(this.bought.indexOf(product), 1);
     }
 }
 
