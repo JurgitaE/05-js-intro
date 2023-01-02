e41616d4a83b60411d76c6281096b25bfabb13ba
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-02 18:20:32 +0200

    ND: intro, nupirkta methods

--- kitchen/Kitchen.js
+++ kitchen/Kitchen.js
@@ -1,5 +1,17 @@
 class Kitchen {
-    constructor() {}
+    constructor(name) {
+        this.name = name;
+        this.bought = [];
+    }
+    intro() {
+        console.log(`Sveiki atvyke i ${this.name} virtuve!`);
+    }
+    nupirkta(pr, quant) {
+        this.bought.push({ product: pr, units: quant });
+    }
+    turimiProduktai() {
+        for (let i = 0; i < this.bought.length; i++) {}
+    }
 }
 
 export default Kitchen;
