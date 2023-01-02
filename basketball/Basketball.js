3ce9df35c0c3f82f04900b59e2bc6136e9551a48
Jurgita <jurgita.eicinaite@yahoo.com>
2023-01-02 21:36:47 +0200

    teamValue

--- basketball/Basketball.js
+++ basketball/Basketball.js
@@ -12,16 +12,20 @@ class Basketball {
         console.log(`A "${name}" just entered a game!`);
     }
     buyPlayer(teamNo, playerNo) {
-        if (this.teams[teamNo - 1].players.length === 3) {
-            console.log(`"${this.teams[teamNo + 1].team}" team can't add extra players to it's team.\nMaximum players per team is 3.`);
-        } else if (this.teams[teamNo - 1].players.includes(this.playerList[playerNo - 1])) {
+        if (this.teams[teamNo - 1].players.includes(this.playerList[playerNo - 1])) {
             console.log(`"${this.teams.team}" team can't add the same player twice!`);
+        } else if (this.teams[teamNo - 1].players.length === 3) {
+            console.log(`"${this.teams[teamNo - 1].team}" team can't add extra players to it's team.\nMaximum players per team is 3.`);
         } else {
             this.teams[teamNo - 1].players.push(this.playerList[playerNo - 1]);
             console.log(
                 `"${this.teams[teamNo - 1].team}" team just acquired new player ${this.playerList[playerNo - 1].player} for ${this.playerList[playerNo - 1].price} cash/year!`
             );
         }
+    }
+    teamValue(teamNo) {
+        const total = this.teams[teamNo - 1].players.reduce((acc, cur) => acc + cur.price, 0);
+        console.log(`"${this.teams[teamNo - 1].team}" team is paying ${total} cash/year for it's players.`);
     }
 }
 
